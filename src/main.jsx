e558af44413825af9ee90a7a064f3db35660b99d
Kajal Sharma <divyansh_213042@saitm.org>
2025-08-12 17:27:55 +0530

    Clerk Authentication

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,44 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
+import { Home } from 'lucide-react'
+import Dashboard from './dashboard'
+import { ClerkProvider } from '@clerk/clerk-react'
 
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+const router = createBrowserRouter([
+  {
+    
+    element:<App/>,
+    children:[
+     
+      {
+        path:'/dashboard',
+        element:<Dashboard/>
+      }
+    ]
+
+  },
+   {
+        path:'/',
+        element:<Home/>
+      },
+  {
+    path:'/auth/sign-in',
+    element:<SignInPage/>
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <RouterProvider router={router} />
+  </ClerkProvider>
   </StrictMode>,
 )
